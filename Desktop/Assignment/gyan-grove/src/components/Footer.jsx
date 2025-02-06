b739576e1804cb32ed58cbbfda07137ab7cb9f3f
Anurag Khobragae <anuragk2204@gmail.com>
2025-02-06 18:36:28 +0530

    change

--- Desktop/Assignment/gyan-grove/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-            <div>By Anurag Khobragade email:anuragk2204@gmail.com</div>
-        </footer>
-    )
-}
--- Desktop/Assignment/gyan-grove/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-export default function Header(){
-    return (
-        <header><img src="/image.png" alt="" /> Dynamic Inventory Management Table</header>
-    );
-}
